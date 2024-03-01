1d1235b8186eab1805d719971bab56b91870ab6a
patryk.labuz <patryk.labuz@S50-11>
2024-03-01 10:29:34 +0100

    Dodanie funkcji dobierania karty

--- BlackjackPLKK/BlackjackPLKK/MainPage.xaml.cs
+++ BlackjackPLKK/BlackjackPLKK/MainPage.xaml.cs
@@ -12,9 +12,77 @@ namespace BlackjackPLKK
     {
         public string[] Values = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
         public string[] Colors = { "♠", "♥", "♣", "♦" };
+        public int UserScoreSum = 0;
+        public int EnemyScoreSum = 0;
+        public Dictionary<string, int> CardValueDictionary = new Dictionary<string, int>()
+        {
+            {"A", 1},
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"5", 5},
+            {"6", 6},
+            {"7", 7},
+            {"8", 8},
+            {"9", 9},
+            {"10", 10},
+            {"J", 11},
+            {"Q", 12},
+            {"K", 13}
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
         }
+
+        public void DrawCardUser()
+        {
+            //Random
+            Random rand = new Random();
+
+            int cardValueIndex = rand.Next(0, 12);
+            int cardColorIndex = rand.Next(0, 3);
+
+            Card card = new Card();
+            card.CardValue = Values[cardValueIndex];
+            card.CardColor = Colors[cardColorIndex];
+
+            //Labels
+            Label cardColorLbl = new Label()
+            {
+                Text = card.CardColor.ToString(),
+                FontSize = 16,
+                HorizontalTextAlignment = TextAlignment.Start, 
+                VerticalTextAlignment = TextAlignment.Start,
+
+            };
+            Label cardValueLbl = new Label()
+            {
+                Text = card.CardValue.ToString(),
+                FontSize = 24,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+            };
+
+
+            //Layout
+            StackLayout UserCardLayout = new StackLayout()
+            {
+                WidthRequest = 70,
+                HeightRequest = 100,
+                BackgroundColor = Color.White,
+                Orientation = StackOrientation.Horizontal,
+                Margin = new Thickness(0,0,10,0),
+                HorizontalOptions = LayoutOptions.Start
+            };
+
+            UserCardLayout.Children.Add(cardColorLbl);
+            UserCardLayout.Children.Add(cardValueLbl);
+
+
+        }
+
     }
 }
